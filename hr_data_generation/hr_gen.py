c9238db67f28847e56729f01b3a2fc2f6b9b19af
Pradeep Janakiraman <pradeepj@Pradeeps-MacBook-Pro.local>
2026-01-19 22:26:53 +0800

    Save org data csv file and add employee assignment function

--- hr_data_generation/hr_gen.py
+++ hr_data_generation/hr_gen.py
@@ -1,6 +1,7 @@
 import numpy as np
 from datetime import date, timedelta
 import random
+import pandas as pd
 
 def get_age_gender(employee_data: dict): 
     age = np.random.randint(low=employee_data["age_min"], high=employee_data["age_max"])
@@ -15,6 +16,18 @@ def get_age_gender(employee_data: dict):
 
     return age, gender
 
+def get_emp_type_country(employee_data: dict): 
+    roll = np.random.rand()
+    if roll < 0.9: 
+        emp_type = "Full time"
+    elif roll < 0.95: 
+        emp_type = "Contract"
+    else: 
+        emp_type = "Part time"
+
+    country = random.choice(employee_data["countries"])
+    
+    return emp_type, country
 
 def get_name(gender: str, employee_data: dict): 
     if gender == "female": 
@@ -48,12 +61,47 @@ def generate_employees(n, employee_data):
         age, gender = get_age_gender(employee_data)
         first_name, last_name = get_name(gender, employee_data)
         hiring_date = get_hiring_date(age)
+        emp_type, country = get_emp_type_country(employee_data)
         employees[i+1] = {
             'age': age, 
             'gender': gender, 
             'first_name': first_name, 
             'last_name': last_name, 
-            'hiring_date': hiring_date
+            'hiring_date': hiring_date, 
+            'country': country, 
+            'employment_type': emp_type
         }
     
     return employees
+
+def _get_assignment_proportions(n_assignments): 
+    r_nums = np.array([np.random.rand() for _ in n_assignments])
+    r_nums_sum = r_nums.sum()
+    r_num_norms = r_nums/r_nums_sum
+
+    return list(r_num_norms)
+
+def assign_employees_to_org(employees: dict, org_data: pd.DataFrame): 
+    employee_assigments = []
+    orgs = org_data["org_id"].to_list()
+    t = date.today()
+    for employee in employees.keys(): 
+        employee_assigment = {}
+        num_assignments = random.choice([1, 2, 3, 4])
+        props = _get_assignment_proportions(num_assignments)
+        delta = t - employees[employee]["hiring_date"]
+        s_date = employees[employee]["hiring_date"]
+        for prop in props: 
+            employee_assigment["employee_id"] = employee
+            employee_assigment["org_id"] = np.random.choice(orgs)
+            employee_assigment["start_date"] = s_date
+            e_date = s_date + prop*delta 
+            employee_assigment["end_date"] = e_date
+            s_date = e_date
+            employee_assigments.append(employee_assigment)
+    return employee_assigments
+            
+        
+
+
+        
